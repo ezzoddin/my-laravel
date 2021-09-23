d4130a7b1d09fc14cca186a9789ddda6e96aa87b
mezz <mobina.ezzoddin@gmail.com>
2021-09-23 12:50:18 +0330

    added todos.show routes for show method

--- laravel-8-todo/routes/web.php
+++ laravel-8-todo/routes/web.php
@@ -15,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [TodoController::class, 'index'])->name("todos.index");
+
+Route::get('/todo/{id}', [TodoController::class, 'show'])->name("todos.show");
