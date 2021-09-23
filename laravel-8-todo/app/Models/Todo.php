83914ba5cfde0f5613454e05ccc33bf111b87472
mezz <mobina.ezzoddin@gmail.com>
2021-09-23 10:42:20 +0330

    added fillable field

--- laravel-8-todo/app/Models/Todo.php
+++ laravel-8-todo/app/Models/Todo.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'completed'
+    ];
+
+
 }
