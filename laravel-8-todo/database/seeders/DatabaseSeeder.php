04b6594b07ae237fe586fd52f8970cdd0fa5ff7f
mezz <mobina.ezzoddin@gmail.com>
2021-09-25 22:51:01 +0330

    run user seeder 'before' todo seeder

--- laravel-8-todo/database/seeders/DatabaseSeeder.php
+++ laravel-8-todo/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        // important , first call user seeder, then Todo seeder
+
+        $this->call(UserSeeder::class);
         $this->call(TodoSeeder::class);
     }
 }
