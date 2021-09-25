247cd9c2f06e1dc9bdce3593d3a2a5cc907f4b33
mezz <mobina.ezzoddin@gmail.com>
2021-09-25 22:49:40 +0330

    define user_id field by User Model random id

--- laravel-8-todo/database/factories/TodoFactory.php
+++ laravel-8-todo/database/factories/TodoFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Todo;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Ybazli\Faker\Facades\Faker;
 
@@ -29,6 +30,7 @@ class TodoFactory extends Factory
             'title' => Faker::word(),
             'description' => Faker::paragraph(),
             'completed' => $this->faker->boolean,
+            'user_id' => User::all()->random()->id,
         ];
 
     }
