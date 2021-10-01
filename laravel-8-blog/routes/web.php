0da1d127f6f53b9828344592c658e788009ad338
mezz <mobina.ezzoddin@gmail.com>
2021-10-01 19:16:02 +0330

    added profile route

--- laravel-8-blog/routes/web.php
+++ laravel-8-blog/routes/web.php
@@ -14,4 +14,8 @@ Route::get('/post/{id}', function ($id) {
     return view('post');
 })->name('post.show');
 
+Route::get('/profile', function () {
+    return 'profile';
+})->name('profile');
+
 require __DIR__ . '/auth.php';
