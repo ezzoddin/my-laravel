f65d10d4117e86fae42125c2404fb7d578ec851d
mezz <mobina.ezzoddin@gmail.com>
2021-10-01 18:29:34 +0330

    added auth blade file

--- laravel-8-blog/resources/views/auth/register.blade.php
+++ laravel-8-blog/resources/views/auth/register.blade.php
@@ -8,18 +8,57 @@
     <div class="container">
         <div class="sign-page">
             <h1 class="sign-page__title">ثبت نام در وب‌سایت</h1>
-            
-            @foreach($errors->all() as $error)
-                <p>{{ $error }}</p>
-            @endforeach
 
             <form class="sign-page__form" action="{{ route('register.store') }}" method="POST">
                 @csrf
-                <input type="text" name="name" class="text text--right" placeholder="نام و نام خانوادگی" />
-                <input type="text" name="mobile" class="text text--left" placeholder="شماره موبایل" />
-                <input type="text" name="email" class="text text--left" placeholder="ایمیل" />
-                <input type="password" name="password" class="text text--left" placeholder="رمز عبور" />
-                <input type="password" name="password_confirmation" class="text text--left" placeholder="تکرار رمز عبور" />
+                <div>
+                    <input type="text" name="name" class="text text--right" placeholder="نام و نام خانوادگی" />
+                    @error('name')
+                        <p style="margin-bottom: 1rem;
+                                color: #D8000C;
+                                text-align: right;"
+                        >
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div>
+                    <input type="text" name="mobile" class="text text--left" placeholder="شماره موبایل" />
+                    @error('mobile')
+                        <p style="margin-bottom: 1rem;
+                                color: #D8000C;
+                                text-align: right;"
+                        >
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+                <div>
+                    <input type="text" name="email" class="text text--left" placeholder="ایمیل" />
+                    @error('email')
+                        <p style="margin-bottom: 1rem;
+                                color: #D8000C;
+                                text-align: right;"
+                        >
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+                <div>
+                    <input type="password" name="password" class="text text--left" placeholder="رمز عبور" />
+                    @error('password')
+                        <p style="margin-bottom: 1rem;
+                                color: #D8000C;
+                                text-align: right;"
+                        >
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+                <div>
+                    <input type="password" name="password_confirmation" class="text text--left" placeholder="تکرار رمز عبور" />
+                </div>
 
                 <button class="btn btn--blue btn--shadow-blue width-100 mb-10" type="submit">
                     ثبت نام
