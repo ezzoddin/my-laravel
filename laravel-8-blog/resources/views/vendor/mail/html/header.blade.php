57bb30d8a25db9b1d37700bfe03037d67fd2f81a
mezz <mobina.ezzoddin@gmail.com>
2021-10-02 19:08:02 +0330

    customize logo in mail header

--- laravel-8-blog/resources/views/vendor/mail/html/header.blade.php
+++ laravel-8-blog/resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+<img src="{{ asset('blog/img/weblogo.png') }}" style="height: auto !important;width: 100% !important;"
 @else
 {{ $slot }}
 @endif
