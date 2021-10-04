85e40bfb4ad89a1f67d7491ab7859918a53129f1
mezz <mobina.ezzoddin@gmail.com>
2021-10-04 17:42:21 +0330

    form update

--- laravel-8-blog/resources/views/panel/users/create.blade.php
+++ laravel-8-blog/resources/views/panel/users/create.blade.php
@@ -1,27 +1,27 @@
 <x-panel-layout>
-  <x-slot name="title">
-    - ساخت کاربر جدید
-  </x-slot>
-  <div class="breadcrumb">
-      <ul>
-          <li><a href="{{ route('dashboard') }}" title="پیشخوان">پیشخوان</a></li>
-          <li><a href="{{ route('users.index') }}" class="">کاربران</a></li>
-          <li><a href="{{ route('users.create') }}" class="is-active">ایجاد کاربر جدید</a></li>
-      </ul>
+    <x-slot name="title">
+        - ساخت کاربر جدید
+    </x-slot>
+    <div class="breadcrumb">
+        <ul>
+            <li><a href="{{ route('dashboard') }}" title="پیشخوان">پیشخوان</a></li>
+            <li><a href="{{ route('users.index') }}" class="">کاربران</a></li>
+            <li><a href="{{ route('users.create') }}" class="is-active">ایجاد کاربر جدید</a></li>
+        </ul>
     </div>
     <div class="main-content font-size-13">
         <div class="row no-gutters  bg-white">
             <div class="col-12">
                 <p class="box__title">ساخت کاربر</p>
-                <form action="" class="padding-30" method="post">
-                    <input type="text" class="text" placeholder="نام و نام خانوادگی">
-                    <input type="text" class="text" placeholder="ایمیل">
-                    <input type="text" class="text" placeholder="شماره موبایل">
-                    <input type="text" class="text" placeholder="آی پی" value="102.111.11" disabled>
-                    <select name="" id="">
-                        <option value="0">کاربر عادی</option>
-                        <option value="2">نویسنده</option>
-                        <option value="3">مدیر</option>
+                <form action="{{ route('users.store') }}" class="padding-30" method="post">
+                    @csrf
+                    <input name="name" type="text" class="text" placeholder="نام و نام خانوادگی">
+                    <input name="email" type="email" class="text" placeholder="ایمیل">
+                    <input name="mobile" type="text" class="text" placeholder="شماره موبایل">
+                    <select name="role">
+                        <option value="user" selected>کاربر عادی</option>
+                        <option value="author">نویسنده</option>
+                        <option value="admin">مدیر</option>
                     </select>
                     <button class="btn btn-webamooz_net">ساخت</button>
                 </form>
