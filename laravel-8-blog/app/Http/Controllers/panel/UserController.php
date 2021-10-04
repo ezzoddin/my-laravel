9d82defcee59a9e7d7fc41f149b93a5b98c6cc9a
mezz <mobina.ezzoddin@gmail.com>
2021-10-04 17:11:00 +0330

    update methods and add veiws

--- laravel-8-blog/app/Http/Controllers/panel/UserController.php
+++ laravel-8-blog/app/Http/Controllers/panel/UserController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Panel;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -9,6 +10,35 @@ class UserController extends Controller
     public function index()
     {
         return view('panel.users.index');
+    }
 
+    public function create()
+    {
+        return view('panel.users.create');
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        return view('panel.users.edit');
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
     }
 }
