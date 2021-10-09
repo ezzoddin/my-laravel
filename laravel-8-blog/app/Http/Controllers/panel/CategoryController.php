19ad10b315f0acb3d5ee2d3685afa3a2e06b1263
mezz <mobina.ezzoddin@gmail.com>
2021-10-09 23:13:44 +0330

    chande directory and delete useless method

---
+++ laravel-8-blog/app/Http/Controllers/panel/CategoryController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Panel;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Category $category)
+    {
+        //
+    }
+
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
