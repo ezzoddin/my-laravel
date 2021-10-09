9ef44e2152bff3987ff62c70825fa9948abe659e
mezz <mobina.ezzoddin@gmail.com>
2021-10-09 23:39:38 +0330

    dlt duplicate code

--- laravel-8-blog/resources/views/_partials/panel-sidebar.blade.php
+++ laravel-8-blog/resources/views/_partials/panel-sidebar.blade.php
@@ -3,17 +3,14 @@
     </li>
     @if(auth()->user()->role === 'admin')
         <li class="item-li i-users @if(request()->is('panel/users') || request()->is('panel/users/*')) is-active @endif">
-            <a href="{{ route('users.index') }}"> کاربران</a></li>
-    @endif
-    <li class="item-li i-categories">
+            <a href="{{ route('users.index') }}"> کاربران</a>
+        </li>
 
-    @if(auth()->user()->role === 'admin')
         <li class="item-li i-categories @if(request()->is('panel/categories') || request()->is('panel/categories/*')) is-active @endif">
             <a href="{{ route('categories.index') }}"> دسته بندی ها</a>
         </li>
     @endif
-
-
+    
     <li class="item-li i-articles"><a href="articles.html">مقالات</a></li>
     <li class="item-li i-comments"><a href="comments.html"> نظرات</a></li>
     <li class="item-li i-user__inforamtion"><a href="user-information.html">اطلاعات کاربری</a></li>
