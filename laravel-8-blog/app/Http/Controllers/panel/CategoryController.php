eb1451698f35efaa240e20a6aab1af7c8695a494
mezz <mobina.ezzoddin@gmail.com>
2021-10-10 20:55:14 +0330

    store method complete

--- laravel-8-blog/app/Http/Controllers/panel/CategoryController.php
+++ laravel-8-blog/app/Http/Controllers/panel/CategoryController.php
@@ -2,21 +2,35 @@
 
 namespace App\Http\Controllers\Panel;
 
+use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+
 
 class CategoryController extends Controller
 {
 
     public function index()
     {
-        //
+        return view('panel.categories.index');
     }
 
 
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'slug' => ['required', 'string', 'max:255', 'unique:categories'],
+            'category_id' => ['nullable', 'exists:categories,id']
+        ]);
+
+        Category::create(
+            $request->only(['name', 'slug', 'category_id'])
+        );
+
+        session()->flash('status', 'دسته بندی مد نظر به درستی ایجاد شد.');
+
+        return back();
     }
 
 
