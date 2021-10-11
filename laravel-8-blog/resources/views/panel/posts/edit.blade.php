e1ba27e05dfdb355a7dab44869867b8712b6f751
mezz <mobina.ezzoddin@gmail.com>
2021-10-11 15:36:46 +0330

    added CKedito

--- laravel-8-blog/resources/views/panel/posts/edit.blade.php
+++ laravel-8-blog/resources/views/panel/posts/edit.blade.php
@@ -17,9 +17,9 @@
                     <ul class="tags">
 
                         <li class="addedTag">dsfsdf<span class="tagRemove" onclick="$(this).parent().remove();">x</span>
-                            <input type="hidden" value="dsfsdf" name="tags[]"></li>
+                            <input type="hidden" value="dsfsdf" name="categories[]"></li>
                         <li class="addedTag">dsfsdf<span class="tagRemove" onclick="$(this).parent().remove();">x</span>
-                            <input type="hidden" value="dsfsdf" name="tags[]"></li>
+                            <input type="hidden" value="dsfsdf" name="categories[]"></li>
                         <li class="tagAdd taglist">
                             <input type="text" id="search-field">
                         </li>
