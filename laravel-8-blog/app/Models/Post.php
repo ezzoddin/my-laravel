3e8285ab30fe9447a7e48a57025a32392b022b14
mezz <mobina.ezzoddin@gmail.com>
2021-10-11 17:54:34 +0330

    sluggble packge added

--- laravel-8-blog/app/Models/Post.php
+++ laravel-8-blog/app/Models/Post.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'banner',
+        'user_id'
+    ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
