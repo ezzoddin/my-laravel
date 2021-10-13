60daaad951045d867574fdc0912ebfa35f3a38e5
mezz <mobina.ezzoddin@gmail.com>
2021-10-13 21:08:40 +0330

    added functions

--- laravel-8-blog/app/Models/Comment.php
+++ laravel-8-blog/app/Models/Comment.php
@@ -8,4 +8,40 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'content',
+        'is_approve',
+        'user_id',
+        'comment_id',
+        'post_id'
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function getCreatedAtInJalali()
+    {
+        return verta($this->created_at)->format('Y/m/d');
+    }
+
+    public function getStatusInFarsi()
+    {
+        return !!$this->is_approved ?
+            'تایید شده' :
+            'تایید شنده';
+    }
 }
