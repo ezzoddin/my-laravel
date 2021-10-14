e9499dc3284012d2370765eb30e63950d02d8ca3
mezz <mobina.ezzoddin@gmail.com>
2021-10-14 14:25:51 +0330

    category composer added

--- laravel-8-blog/app/Providers/ViewServiceProvider.php
+++ laravel-8-blog/app/Providers/ViewServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Http\View\Composers\CategoryComposer;
 
 class ViewServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,6 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('layouts.app', CategoryComposer::class);
     }
 }
