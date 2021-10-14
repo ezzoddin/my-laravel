6392ef071ff73e24a5a303ad0078b9f34f107d15
mezz <mobina.ezzoddin@gmail.com>
2021-10-14 16:21:20 +0330

    show post with user,categories,comments

---
+++ laravel-8-blog/app/Http/Controllers/ShowPostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class ShowPostController extends Controller
+{
+    public function show(Post $post)
+    {
+        $post->load(['user', 'categories', 'comments' => function ($query) {
+            return $query->where('comment_id', null);
+        }]);
+        return view('post', compact('post'));
+    }
+}
