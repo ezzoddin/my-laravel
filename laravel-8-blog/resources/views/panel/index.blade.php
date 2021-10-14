e3cf07da375b6bc82c9b74d2f357263a861ec668
mezz <mobina.ezzoddin@gmail.com>
2021-10-14 13:42:40 +0330

    show info

--- laravel-8-blog/resources/views/panel/index.blade.php
+++ laravel-8-blog/resources/views/panel/index.blade.php
@@ -1,30 +1,37 @@
 <x-panel-layout>
-<x-slot name="title">
-  - داشبورد
-</x-slot>
-<div class="breadcrumb">
-    <ul>
-        <li><a href="{{ route('dashboard') }}" title="پیشخوان">پیشخوان</a></li>
-    </ul>
+    <x-slot name="title">
+        - داشبورد
+    </x-slot>
+    <div class="breadcrumb">
+        <ul>
+            <li><a href="{{ route('dashboard') }}" title="پیشخوان">پیشخوان</a></li>
+        </ul>
     </div>
     <div class="main-content">
         <div class="row no-gutters font-size-13 margin-bottom-10">
-            <div class="col-3 padding-20 border-radius-3 bg-white margin-left-10 margin-bottom-10">
-                <p> تعداد کاربران </p>
-                <p>20 نفر</p>
-            </div>
-            <div class="col-3 padding-20 border-radius-3 bg-white margin-left-10 margin-bottom-10">
-                <p>تعداد پست ها</p>
-                <p>20 پست</p>
-            </div>
-            <div class="col-3 padding-20 border-radius-3 bg-white margin-left-10 margin-bottom-10">
-                <p>تعداد نظرات</p>
-                <p>300 نظر</p>
-            </div>
-            <div class="col-3 padding-20 border-radius-3 bg-white  margin-bottom-10">
-                <p>تعداد دسته بندی ها</p>
-                <p>300 نظر</p>
-            </div>
+
+            @if(auth()->user()->role === 'admin' || auth()->user()->role === 'author' )
+                <div class="col-3 padding-20 border-radius-3 bg-white margin-left-10 margin-bottom-10">
+                    <p>تعداد پست ها</p>
+                    <p>{{ $posts_count }}</p>
+                </div>
+                <div class="col-3 padding-20 border-radius-3 bg-white margin-left-10 margin-bottom-10">
+                    <p>تعداد نظرات</p>
+                    <p>{{ $comments_count }}</p>
+                </div>
+            @endif
+
+            @if(auth()->user()->role === 'admin')
+                <div class="col-3 padding-20 border-radius-3 bg-white margin-left-10 margin-bottom-10">
+                    <p> تعداد کاربران </p>
+                    <p>{{ $users_count }}</p>
+                </div>
+                <div class="col-3 padding-20 border-radius-3 bg-white  margin-bottom-10">
+                    <p>تعداد دسته بندی ها</p>
+                    <p> {{ $categories_count }}</p>
+                </div>
+            @endif
+
         </div>
 
     </div>
