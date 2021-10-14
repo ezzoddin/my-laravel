72b3ec87512424d580c219528e23dd6e826df4e7
mezz <mobina.ezzoddin@gmail.com>
2021-10-14 14:51:42 +0330

    get banner url added

--- laravel-8-blog/app/Models/Post.php
+++ laravel-8-blog/app/Models/Post.php
@@ -42,4 +42,9 @@ class Post extends Model
         return verta($this->created_at)->format('Y/m/d');
     }
 
+    public function getBannerUrl()
+    {
+        return asset('images/banners/'. $this->banner);
+    }
+
 }
